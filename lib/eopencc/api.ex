96301e4896ae0a09eb1eb9168afaa93055c07509
Tim Lang <langyong135@gmail.com>
2016-11-17 17:03:08 +0800

    add Simplified Chinese to Traditional Chinese (Taiwan Standard) with Taiwanese idiom and Traditional Chinese (Taiwan Standard) to Simplified Chinese with Mainland Chinese idiom

--- lib/eopencc/api.ex
+++ lib/eopencc/api.ex
@@ -10,6 +10,14 @@ defmodule Eopencc.Api do
     _translate(:t2s, str)
   end
 
+  def s2twp str do
+    _translate(:s2twp, str)
+  end
+
+  def tw2sp str do
+    _translate(:tw2sp, str)
+  end
+
   defp _translate op, str do
     worker = :poolboy.checkout(:eopencc_pool)
     {w, h} = Worker.call_ruby(worker, op, [str])
