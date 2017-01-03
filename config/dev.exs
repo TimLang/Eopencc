1d143d601bac80a6ec0c7001c8285ee8c74fc280
Tim Lang <langyong135@gmail.com>
2017-01-03 10:40:25 +0800

    Fix config cant be overridded

--- config/dev.exs
+++ config/dev.exs
@@ -1,2 +1,16 @@
 
 use Mix.Config
+
+config :eopencc, settings: [
+  # Shoud not be the system ruby in OSX
+  {:ruby, 'ruby'},
+  {:module_name, :ropencc_wrapper},
+  # Poolboy configurations
+  {:poolboy, [
+    name: {:local, :eopencc_pool},
+    worker_module: Eopencc.Worker,
+    size: 10,
+    max_overflow: 20
+  ]}
+]
+
