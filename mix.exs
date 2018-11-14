e376453c66395ed3fb3e147778d3d15e00ca7f01
Tim Lang <langyong135@gmail.com>
2018-11-14 17:21:11 +0800

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eopencc.Mixfile do
 
   def project do
     [app: :eopencc,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
